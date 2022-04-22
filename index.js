c2b369309c48d054a286c35c7bba07384a0e8fe0
McKB <mckennakb@comcast.net>
2022-04-22 08:17:52 -0400

    plans out calculate() function

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const calculate = () => {
+  // initialize operator variable
+  // initialize nums array
+  // initialize answer variable
+
+  // loop over incoming array
+  // if value is a number, store it in an array called nums
+  // if value is an operator, store it in a variable called operator
+  // if value is 'NULL', add it to nums as 0
+  // if none of those, move onto the next item and ignore the current item.
+
+  // if the nums array has two values in it:
+  //   if operator is +, add the values & store it in answer
+  //   if operator is -, subtract the values & store it in answer
+  //   if operator is *, multiply the values & store it in answer
+  //   if operator is /, divide the values & store it in answer
+  //   if there is no operator, answer = NaN
+  // if the nums array does not have two values in it, answer = NaN
+
+  // return answer
 
 }
 
